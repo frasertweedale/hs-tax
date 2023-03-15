5ecb192b3dfc9a46c0eb40ba8f8e68475ec8fc42
Fraser Tweedale <frase@frase.id.au>
2023-03-15 14:22:14 +1100

    doc: improve module doc

--- src/Data/Tax.hs
+++ src/Data/Tax.hs
@@ -14,11 +14,50 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 {-|
 
 This library provides combinators for constructing taxes.  It is
 based on the <https://hackage.haskell.org/package/dollaridoos dollaridoos>
 library.
+
+-}
+module Data.Tax
+  (
+  -- * Overview
+  -- $doc
+
+  -- * API
+    Tax(..)
+  , MoneyTax
+  , lump
+  , flat
+  , threshold
+  , threshold'
+  , thresholds
+  , above
+  , above'
+  , marginal
+  , lesserOf
+  , greaterOf
+  , limit
+  , effective
+
+  -- * Re-exports
+  , module Data.Money
+  , Semigroup(..)
+  , Monoid(..)
+  , Profunctor(..)
+  ) where
+
+import Data.Monoid (Monoid(..))
+import Data.Profunctor (Profunctor(..))
+import Data.Semigroup (Semigroup(..))
+
+import Data.Money
+
+{- $doc
 
 The most basic tax is a flat rate tax:
 
@@ -29,31 +68,41 @@ businessTax = 'flat' 0.3
 To compute the tax, use 'getTax':
 
 @
-λ> 'getTax' businessTax (review money 1000000)
+λ> 'getTax' businessTax (Money 1000000)
 $300000.0
 @
 
 Taxes form a semigroup (sum of tax outputs) and monoid:
 
 @
-λ> getTax (flat 0.1 <> flat 0.2) (review money 10)
+λ> getTax (flat 0.1 <> flat 0.2) (Money 10)
 $3.0
-λ> getTax mempty (review money 10)
+λ> getTax mempty (Money 10)
 $0
 @
 
-Marginal tax rates can be constructed using the 'above' combinator,
+Progressive taxes can be constructed using the 'above' combinator,
 which taxes the amount above a given threshold at a flat rate.
 
 @
 individualIncomeTax =
-  'above' (review money 18200) 0.19
-  <> 'above' (review money 37000) (0.325 - 0.19)
-  <> 'above' (review money 87000) (0.37 - 0.325)
-  <> 'above' (review money 180000) (0.45 - 0.37)
+     'above' (Money 18200 ) (0.19  - 0     )
+  <> 'above' (Money 45000 ) (0.325 - 0.19  )
+  <> 'above' (Money 120000) (0.37  - 0.325 )
+  <> 'above' (Money 180000) (0.45 - 0.37   )
 @
 
-Taxes can be negative.  For exmaple, the 'lump', 'above' and 'limit'
+The 'marginal' function provides a shorthand for the above.
+
+@
+individualIncomeTax = 'marginal'
+  [ ( Money 18200,  0.19  - 0     )
+  , ( Money 45000,  0.325 - 0.19  )
+  , ( Money 120000, 0.37  - 0.325 )
+  , ( Money 180000, 0.45  - 0.37  ) ]
+@
+
+Taxes can be negative.  For example, the 'lump', 'above' and 'limit'
 combinators can be used to construct a low-income tax offset that
 starts at $445 and reduces at a rate of 1.5c per dollar earned over
 $37000:
@@ -61,19 +110,26 @@ $37000:
 @
 lowIncomeTaxOffset =
   'limit' mempty
-  ('lump' (review money (-445)) <> 'above' (review money 37000) 0.015)
+  ('lump' (Money (-445)) <> 'above' (Money 37000) 0.015)
 @
 
 The 'threshold' combinator applies a tax to the full input amount,
-if it exceeds the threshold.  Some taxes have "shade-in" where the
-amount above the threshold is taxed at a higher rate to "catch up"
-to some lower flat rate.  The 'threshold'' and 'lesserOf'
-combinators can be used to construct this tax:
+if it exceeds the threshold.
 
 @
-medicareLevy =
-  'threshold'' l ('lesserOf' ('above' l 0.1) ('flat' 0.02))
-    where l = review money 21656
+medicareLevySurcharge =
+     'threshold' (review money 90000 ) 0.0100
+  <> 'threshold' (review money 105000) 0.0025
+  <> 'threshold' (review money 140000) 0.0025
+@
+
+Some taxes have "shade-in" where the amount above some threshold is
+taxed at a higher rate to "catch up" to some lower flat rate.  The
+'above' and 'lesserOf' combinators can be used to construct this
+tax:
+
+@
+medicareLevy l = 'lesserOf' ('above' l 0.1) ('flat' 0.02)
 @
 
 Although some of the combinators deal directory with 'Money', a
@@ -95,40 +151,8 @@ corvée = Tax f
 
 -}
 
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
-module Data.Tax
-  (
-  -- * Constructing taxes
-    Tax(..)
-  , MoneyTax
-  , lump
-  , flat
-  , threshold
-  , threshold'
-  , thresholds
-  , above
-  , above'
-  , marginal
-  , lesserOf
-  , greaterOf
-  , limit
-  , effective
-
-  -- * Miscellanea
-  , Semigroup(..)
-  , Monoid(..)
-  , Profunctor(..)
-  , module Data.Money
-  ) where
-
-import Data.Monoid (Monoid(..))
-import Data.Profunctor (Profunctor(..))
-import Data.Semigroup (Semigroup(..))
-
-import Data.Money
-
--- | A function from gross income to tax payable.
+-- | A function from an amount or value subject to taxation, to
+-- the amount or value of tax due.
 --
 -- Taxes form a semigroup where the tax payable is the
 -- sum of tax payable of consituent taxes.
@@ -154,7 +178,7 @@ above l = above' l . flat
 above' :: (Num b, Ord b) => Money b -> Tax (Money b) a -> Tax (Money b) a
 above' l = lmap (\x -> max (x $-$ l) mempty)
 
--- | Convert a @[(threshold, rate)]@ into a marginal tax.
+-- | Convert a @[(threshold, rate)]@ into a progressive tax.
 -- The rates are /cumulative/, i.e. the top marginal rate is the
 -- sum of the rates that apply for a given input.
 --
@@ -162,7 +186,7 @@ marginal :: (Num a, Ord a) => [(Money a, a)] -> Tax (Money a) (Money a)
 marginal = foldMap (uncurry above)
 
 
--- | A lump-sum tax; a fixed value, not affected by the size of the input
+-- | A lump-sum tax; a fixed value (ignores input).
 --
 lump :: a -> Tax b a
 lump = Tax . const
@@ -186,7 +210,7 @@ threshold' l tax = Tax (\x -> if x >= l then getTax tax x else mempty)
 -- extra 10% at 50000:
 --
 -- @
--- tax = thresholds [(30000, .2), (50000, .1)]
+-- tax = thresholds [(30000, 0.2), (50000, 0.1)]
 -- @
 --
 thresholds :: (Num a, Ord a) => [(Money a, a)] -> Tax (Money a) (Money a)
@@ -202,8 +226,8 @@ greaterOf t1 t2 = Tax (\x -> max (getTax t1 x) (getTax t2 x))
 
 -- | Limit the tax payable to the given amount
 --
--- This could be used e.g. for limiting a compulsory loan
--- repayment to the balance of the loan, or ensuring a
+-- This could be used e.g. to limit a compulsory loan
+-- repayment to the balance of the loan, or ensure a
 -- (negative) tax offset does not become a (positive) tax.
 --
 limit :: (Ord a) => a -> Tax b a -> Tax b a
